cc16e1b663f17f9e93e89f6c7099af0f8731d7dc
Julles <juliokirk@gmail.com>
2023-11-20 22:39:36 -0300

    Ajustes em logos

--- js/greeting.js
+++ js/greeting.js
@@ -6,8 +6,8 @@ const hour = today.getHours();
 //const name = 'Captain.';
 
 // Here you can change your greetings
-const gree1 = 'Residômetro Amazon Reuse: Gestão de Resíduos Recicláveis';
-const gree2 = 'Good morning,  ';
+/// const gree1 = 'Residômetro Amazon Reuse: Gestão de Resíduos Recicláveis';
+const gree2 = 'Parceiros:';
 const gree3 = 'Good afternoon,  ';
 const gree4 = 'Good evening,  ';
 const gree5 = 'Good evening,  ';
@@ -15,7 +15,7 @@ const gree6 = 'Good evening,  ';
 
 // Define the hours of the greetings
 if (hour >= 0 && hour <= 24) {
-  document.getElementById('greetings').innerText = gree1;
+  document.getElementById('greetings').innerText = gree2;
 } //else if (hour >= 6 && hour < 12) {
   //document.getElementById('greetings').innerText = gree2 + name;
 //} else if (hour >= 12 && hour < 17) {
